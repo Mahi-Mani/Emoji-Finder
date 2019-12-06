053a22d1f8a3088fcbfa821d9ef07e0cd3a6c13d
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2019-12-06 14:44:02 -0800

    Coded logic to compare existing emojis with user entered value

---
+++ emojiapp/src/components/Display/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Display(props) {
+    console.log(props);
+    return (
+        <div>
+            Hello kitty
+        </div>
+    )
+}
+
+export default Display;
