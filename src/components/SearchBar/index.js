d6cbb481993e8568785797519f9951ea0427569d
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2019-12-10 13:07:38 -0800

    coded logic to dynamically search for emojis

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -22,33 +22,37 @@ class SearchBar extends Component {
         console.log(this.state.searchValue);
         console.log(emoji[0].name);
 
-        for(let i=0; i<emoji.length; i++){
-        if(this.state.searchValue === emoji[i].name) {
-            console.log("Found");
-            alert("Found");
-            <Display>
-                {emoji[i].image}
-            </Display> 
+        for (let i = 0; i < emoji.length; i++) {
+            if (this.state.searchValue === emoji[i].name) {
+                console.log("Found");
+                console.log(emoji[i]);
+                this.setState({ render: !this.state.render,
+                emoji: emoji[i] })
+            }
+            else {
+                console.log("Not found");
+            }
         }
-        else{
-            console.log("Not found");
-        }
-    }
     }
 
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
                 <form className="form-inline">
-                    <input className="form-control mr-sm-2" 
+                    <input className="form-control mr-sm-2"
                         type="search" placeholder="Search" aria-label="Search"
                         name="searchValue"
-                        value = {this.state.searchValue} onChange={this.handleInputChange}
+                        value={this.state.searchValue} onChange={this.handleInputChange}
                     ></input>
-                    <button className="btn btn-outline-success my-2 my-sm-0" 
-                    type="submit"
-                    onClick={this.handleEvent}
+                    <button className="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                        onClick={this.handleEvent}
                     >Search</button>
+                    {this.state.render &&
+                        <Display
+                            search={this.state.searchValue}
+                            emoji={this.state.emoji}
+                        />}
                 </form>
             </nav>
         )
