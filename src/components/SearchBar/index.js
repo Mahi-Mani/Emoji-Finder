e046e355f0082da2ea567704508c6e179687834d
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-01-02 15:02:24 -0800

    Application now dynamically displays emoticon when keys are pressed

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import emoji from "../../emoji.json";
 import Display from "../Display";
+import { DisplayList, DisplayListItem } from "../DisplayList";
 
 class SearchBar extends Component {
     state = {
@@ -9,54 +10,112 @@ class SearchBar extends Component {
 
     handleInputChange = event => {
         event.preventDefault();
-        console.log(event.target);
+        // console.log(event.target);
         const { name, value } = event.target;
         this.setState({
             [name]: value
         })
+
+        emoji.map(icon => {
+            let name = icon.name;
+            let nameLowerCase = name.toLowerCase();
+            let searchValueLowerCase = this.state.searchValue.toLowerCase();
+
+            if(nameLowerCase.includes(searchValueLowerCase)){
+                console.log(`The value matches to true`);
+                console.log(icon.name);
+
+                this.setState({
+                    render: !this.state.render,
+                    emoji: icon
+                })
+            }
+        })
+
+        // for (let i = 0; i < emoji.length; i++) {
+        //     let searchValueLowerCase = this.state.searchValue.toLowerCase();
+        //     let emojiNameLowerCase = emoji[i].name.toLowerCase();
+
+        //     if (searchValueLowerCase === emojiNameLowerCase) {
+        //         console.log("Found");
+
+        //         this.setState({
+        //             render: !this.state.render,
+        //             emoji: emoji[i]
+        //         })
+        //     }
+        // }
     }
 
     handleEvent = event => {
         event.preventDefault();
         console.log("inside submit button function");
-        console.log(this.state.searchValue);
-        console.log(emoji[0].name);
 
         for (let i = 0; i < emoji.length; i++) {
-            if (this.state.searchValue === emoji[i].name) {
+            let searchValueLowerCase = this.state.searchValue.toLowerCase();
+            let emojiNameLowerCase = emoji[i].name.toLowerCase();
+
+            if (searchValueLowerCase === emojiNameLowerCase) {
                 console.log("Found");
-                console.log(emoji[i]);
-                this.setState({ render: !this.state.render,
-                emoji: emoji[i] })
+
+                this.setState({
+                    render: !this.state.render,
+                    emoji: emoji[i]
+                })
             }
-            else {
-                console.log("Not found");
-            }
+            // else {
+            //     console.log("Not found");
+            //     this.setState({ render: !this.state.render,
+            //     emoji: emoji[3] })
+            // }
         }
     }
 
     render() {
         return (
-            <nav className="navbar navbar-light bg-light">
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2"
-                        type="search" placeholder="Search" aria-label="Search"
-                        name="searchValue"
-                        value={this.state.searchValue} onChange={this.handleInputChange}
-                    ></input>
-                    <button className="btn btn-outline-success my-2 my-sm-0"
-                        type="submit"
-                        onClick={this.handleEvent}
-                    >Search</button>
-                    {this.state.render &&
-                        <Display
-                            search={this.state.searchValue}
-                            emoji={this.state.emoji}
-                        />}
-                </form>
-            </nav>
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <form className="form-inline">
+                        <input className="form-control mr-sm-2"
+                            type="search" placeholder="Search" aria-label="Search"
+                            name="searchValue"
+                            value={this.state.searchValue} onChange={this.handleInputChange}
+                        ></input>
+                        <button className="btn btn-outline-success my-2 my-sm-0"
+                            type="submit"
+                            onClick={this.handleEvent}
+                        >Search</button><br></br><br></br><br></br><br></br>
+                        {this.state.render &&
+                            <Display
+                                search={this.state.searchValue}
+                                emoji={this.state.emoji}
+                            />}
+                        <br></br><br></br><br></br><br></br>
+                    </form>
+                </nav>
+                <DisplayList>
+                    {emoji.map(icon => (
+                        <DisplayListItem
+                            key={icon.id}
+                            name={icon.name}
+                            image={icon.image}
+                        />
+                    ))}
+                </DisplayList>
+                {this.state.render &&
+                            <DisplayList>
+                                <DisplayListItem
+                                    key={this.state.emoji.id}
+                                    name={this.state.emoji.name}
+                                    image={this.state.emoji.image}
+                                />
+}
+                        </DisplayList>}
+            </div>
         )
     }
 }
+
+
 
 export default SearchBar;
