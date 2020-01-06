e6bffc5e9cd33c8a85bbeab3705871a64e913d37
Mahisha Gunasekaran <m.mahishamani@gmail.com>
2020-01-06 11:35:02 -0800

    Updates

---
+++ precache-manifest.3a5cf4d3c29ab6c98859ac013060f14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd14a3ee4f0d099f885a06348e0b342f",
+    "url": "/Emoji-Finder/index.html"
+  },
+  {
+    "revision": "c32aff7f1e2af6d51860",
+    "url": "/Emoji-Finder/static/css/main.9865c041.chunk.css"
+  },
+  {
+    "revision": "41bb318abcd109296437",
+    "url": "/Emoji-Finder/static/js/2.a4980cff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Emoji-Finder/static/js/2.a4980cff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c32aff7f1e2af6d51860",
+    "url": "/Emoji-Finder/static/js/main.4e7a1515.chunk.js"
+  },
+  {
+    "revision": "ca3de7d53acc6b77d7a8",
+    "url": "/Emoji-Finder/static/js/runtime-main.d1c1c9a0.js"
+  }
+]);
